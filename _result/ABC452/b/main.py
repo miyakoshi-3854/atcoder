e4fb6f6deff3a74eccc74cff41694f2aba737a6d
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-04-04 21:25:30 +0900

    ABC452 b (main.py)

---
+++ _result/ABC452/b/main.py
@@ -0,0 +1,8 @@
+height, width = map(int, input().split())
+
+conma_width = width - 2
+for i in range(height):
+    if i == 0 or i == height - 1:
+        print("#" * width)
+    else:
+        print("#","." * conma_width,"#", sep="")
