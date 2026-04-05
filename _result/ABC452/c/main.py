19afb32898ddcd2e9966c3f0307bdbd3dc64b016
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-04-05 17:10:03 +0900

    ABC452 c (main.py)

---
+++ _result/ABC452/c/main.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+
+# 入力
+N = int(input())
+A = [0] * N 
+B = [0] * N
+for i in range(N):
+    A[i], B[i] = map(int, input().split())
+M = int(input())
+strings = []
+for i in range(M):
+    strings += input().split()
+
+# Bにくる値をdictにする
+can_place = defaultdict(set)
+for i in range(N):
+    for S in strings:
+        if A[i] == len(S):
+            can_place[A[i], B[i]].add(S[B[i] - 1])
+
+# can_placeに当てはまるsが存在するか
+for S in strings:
+    if len(S) == N:
+        for i in range(N):
+            if S[i] not in can_place[(A[i], B[i])]:
+                print("No")
+                break
+        else:
+            print("Yes")
+    else:
+        print("No")
