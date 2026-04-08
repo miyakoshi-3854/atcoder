315ac4e6d6e87d293884c3148c9b11f88467ee6f
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-04-08 17:34:57 +0900

    ADTE-2026-04-08 c (main.py)

---
+++ _result/ADTE-2026-04-08/c/main.py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+S = [input() for _ in range(N)]
+
+correct_count = 0 
+for i in range(N):
+    for j in range(i + 1, N):
+        is_valid_pair = True
+        for k in range(M):
+            if S[i][k] == "x" and S[j][k] == "x":
+                is_valid_pair = False
+                break
+        if is_valid_pair:
+            correct_count += 1
+
+print(correct_count)
