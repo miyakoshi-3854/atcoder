b143a1debc7540d98a86828704e10a41d2c29445
miyakoshi-3854 <208133564+miyakoshi-3854@users.noreply.github.com>
2026-04-18 21:28:12 +0900

    ABC454 b (main.py)

---
+++ _result/ABC454/b/main.py
@@ -0,0 +1,19 @@
+"""
+条件
+全員着ている服が違う
+m種類の服を最低一枚誰かが着ているか
+"""
+n, m = map(int, input().split())
+f = [int(f) for f in input().split()]
+
+# 来ている服が違う
+if len(f) == len(set(f)):
+    print("Yes")
+else:
+    print("No")
+
+# m種類の服を最低一枚着ているか
+if len(set(f)) == m:
+    print("Yes")
+else:
+    print("No")
